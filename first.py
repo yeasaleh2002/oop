659380ad205609fa099fdfb10811609753a7bc2b
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-03-25 11:18:17 +0600

    dynamic value

--- first.py
+++ first.py
@@ -1,5 +1,14 @@
 print("printing the pythons...")
-# for single line comment use # symbol
+# for single line comment use # symbol - ctrl + /
 '''
-for multiple line comment use triple single quotes
+for multiple line comment use triple single quotes- alt + shift + A
 '''
+
+# Variable declaration
+a = 10
+b = 20
+c = a + b
+print(c)
+print(type(c)) # typecasting
+text = f"The {a} and {b} are the same and value is {c} " # for dynamic values use f
+print(text)
