055f50ecb04c17da6527aa2be49ecd3d61212dae
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-03-26 12:32:55 +0600

    conditional

---
+++ conditional.py
@@ -0,0 +1,19 @@
+a = 4
+if a > 5: 
+    print("a is greater than 5")
+elif a < 5:
+    print("a is less than 5")
+else: 
+    print("a is equal to 5")
+
+boss = False
+
+if boss is True:
+    print("This will be printed")
+else:
+    print("This will not be printed")
+
+if boss is not True:
+    print("This will be printed")
+else:
+    print("This will not be printed")
