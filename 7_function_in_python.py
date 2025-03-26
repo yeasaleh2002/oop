b58879c52e1135b3a17cf23e5c19b073832fe130
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-03-26 15:27:32 +0600

    function

---
+++ 7_function_in_python.py
@@ -0,0 +1,14 @@
+# function in python
+
+def double_it(num):
+    result = num * 2
+    print(result)
+
+double_it(5)
+double_it(10)
+
+def sum(num1, num2):
+    result = num1 + num2
+    return result
+
+sum_result = sum(5, 10)
